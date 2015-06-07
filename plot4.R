d0020ee547e729c9785233e68741957f7e3e6716
dennislrhodes <rhodes.dennisl@gmail.com>
2015-06-07 18:23:03 -0500

    Updated plot4 R file

--- plot4.R
+++ plot4.R
@@ -50,13 +50,14 @@ with(epc, {
         ## adds legend
         legend("topright", legend = c("Sub_metering_1" , "Sub_metering_2" , "Sub_metering_3"), col = c("black","blue","red"),lty = 1, lwd = 2, cex = .30)
       
-        ##creates third graph
-        plot(dt, Voltage, type = "l", xlab = "datetime", ylab = "Voltage")
-        
-        ##creates fourth graph
-        plot(dt, grp, type = "l", xlab = "datetime", ylab = "Global_Reactive_Power")
         
 })
+
+##creates third graph
+plot(dt, Voltage, type = "l", xlab = "datetime", ylab = "Voltage")
+
+##creates fourth graph
+plot(dt, grp, type = "l", xlab = "datetime", ylab = "Global_Reactive_Power")
 
 ## exports graph as png
 dev.copy(png, "plot4.png")
